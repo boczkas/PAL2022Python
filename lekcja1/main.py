033e42fbb7c623b334f1fabdfdd3a9334f036f51
Przemysław <przemyslawjakubowski@gmail.com>
2022-04-13 23:49:37 +0200

    Lekcja1

---
+++ lekcja1/main.py
@@ -0,0 +1,103 @@
+import time
+
+
+def start():
+    # 1. Zawsze
+
+    # https://scratch.mit.edu/projects/675675913/editor/
+    while True:
+        print("Cześć")
+        time.sleep(1)
+
+    # Napisz program, który wypisuje "Siema!" co 2s
+
+    # 2. Jeżeli
+
+    # https://scratch.mit.edu/projects/675677711/editor/
+    # liczba = 5
+    # if liczba > 4:
+    #     print("Liczba większa niż 4")
+
+    # Napisz program, który sprawdza czy liczba jest mniejsza niż 3
+
+    # 3. Jeżeli/w przeciwnym razie
+    # https://scratch.mit.edu/projects/675679820/editor/
+    # liczba = 5
+    # if liczba > 4:
+    #     print("Liczba większa niż 4")
+    # else:
+    #     print("Liczba mniejsza bądź równa 4")
+
+    # Napisz program, który sprawdza czy liczba jest jest większa niż 10.
+    # Jeśli tak, wypisuje "Liczba większa niż 10",  w przeciwnym razie wypisuje "Liczba mniejsza niż 10"
+
+    # 4. for
+    # https://scratch.mit.edu/projects/675687157/editor/
+    # for liczba in range(5):
+    #     print(liczba)
+
+    # 5. Napisz program wykorzystujący pętlę for do przejścia przez liczby od 0 do 9 i sprawdzający za każdym razem
+    # czy liczba jest parzysta czy nie
+    # https://scratch.mit.edu/projects/675688629/editor/
+
+    # liczba = 0
+    # if liczba == 0:
+    #     print("Liczba jest równa 0")
+    #
+    # liczba = 11
+    # if liczba % 2 == 0:
+    #     print("Liczba jest parzysta")
+    # else:
+    #     print("Liczba jest nieparzysta")
+
+    # 6. Operacje arytmetyczne
+    # a = 4
+    # b = 2
+    # c = a + b
+    # print("c = ", c)
+    #
+    # c = a - b
+    # print("c = ", c)
+    #
+    # c = a * b
+    # print("c = ", c)
+    #
+    # c = a / b
+    # print("c = ", c)
+    #
+    # c = a**2
+    # print("c = ", c)
+    #
+
+
+    # "Nigdy mi się nie przyda rozwiązywanie równań kwadratowych!"
+    # 7. Napisz program rozwiązujący równanie kwadratowe (o ile istnieje)
+
+    # https://www.matemaks.pl/rownania-kwadratowe-w-postaci-ogolnej.html
+
+
+
+
+
+    # 8. Wczytywanie danych z klawiatury
+    # https://scratch.mit.edu/projects/664805430/editor/
+
+    # odpowiedz = input("Jak się nazywasz?\n")
+    # print("Cześć!")
+    # print(odpowiedz)
+
+    # Wypisanie w jednej linijce
+    # odpowiedz = input("Jak się nazywasz?\n")
+    # print("Cześć", odpowiedz, "!")
+
+    # 8. Narysuj konkretną ilość kresek
+    # https://scratch.mit.edu/projects/675697553/editor/
+
+    # Gdy chcemy wczytać liczbę z tekstu int("5")
+    # Narazie przyjmijmy to za pewnik, będziemy o tym rozmawiać później
+    # ilosc_kresek = int(input("Ile kresek?\n"))
+    # for i in range(ilosc_kresek):
+    #     print("|")
+
+if __name__ == '__main__':
+    start()
