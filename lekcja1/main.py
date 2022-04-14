e91377f982a1d4519449cb8a1b32e73d925cd170
Przemysław <przemyslawjakubowski@gmail.com>
2022-04-14 17:57:54 +0200

    lekcja1 po lekcji

--- lekcja1/main.py
+++ lekcja1/main.py
@@ -5,10 +5,11 @@ import time
 def start():
     # 1. Zawsze
 
+    # print("Costam")
     # https://scratch.mit.edu/projects/675675913/editor/
-    while True:
-        print("Cześć")
-        time.sleep(1)
+    # while True:
+    #     print("Cześć")
+    #     time.sleep(1)
 
     # Napisz program, który wypisuje "Siema!" co 2s
 
@@ -32,6 +33,65 @@ def start():
     # Napisz program, który sprawdza czy liczba jest jest większa niż 10.
     # Jeśli tak, wypisuje "Liczba większa niż 10",  w przeciwnym razie wypisuje "Liczba mniejsza niż 10"
 
+    print("0 == 1", 0 == 1)
+    liczba = 1
+    if liczba == 0:
+        print("Liczba jest równa 0")
+
+    pierwastek = math.sqrt(4)
+    print(pierwastek)
+
+    # 6. Operacje arytmetyczne
+    # a = 4
+    # b = 2
+    # c = a + b
+    # print("a + b = ", c)
+    #
+    # c = a - b
+    # print("a - b = ", c)
+    #
+    # c = a * b
+    # print("a * b = ", c)
+    #
+    # c = a / b
+    # print("a / b = ", c)
+    #
+    # c = a**2
+    # print("a**2 = ", c)
+
+
+    # "Nigdy mi się nie przyda rozwiązywanie równań kwadratowych!"
+    # 7. Napisz program rozwiązujący równanie kwadratowe (o ile istnieje)
+
+    # https://www.matemaks.pl/rownania-kwadratowe-w-postaci-ogolnej.html
+
+    a = 1
+    b = 2
+    c = -3
+
+    # delta = b^2 - 4ac
+    delta = b**2 - 4 * a * c
+    print(delta)
+
+    if (delta < 0):
+        print("Rownanie kwadratowe nie ma rozwiazania")
+    elif(delta > 0):
+        math.sqrt(delta)
+        print("To macie napisać")
+    else:
+        x = -b / 2*a
+        print("x", x)
+
+
+
+    #  TEGO NIE ZROBILIŚMY W SALI!!!!!!
+
+    # liczba = 10
+    # if liczba % 2 == 0:
+    #     print("Liczba jest parzysta")
+    # else:
+    #     print("Liczba jest nieparzysta")
+
     # 4. for
     # https://scratch.mit.edu/projects/675687157/editor/
     # for liczba in range(5):
@@ -41,59 +101,6 @@ def start():
     # 5. Napisz program wykorzystujący pętlę for do przejścia przez liczby od 0 do 9 i sprawdzający za każdym razem
     # czy liczba jest parzysta czy nie
     # https://scratch.mit.edu/projects/675688629/editor/
-
-    # print(1 == 1)
-    # liczba = 0
-    # if liczba == 0:
-    #     print("Liczba jest równa 0")
-
-    # liczba = 10
-    # if liczba % 2 == 0:
-    #     print("Liczba jest parzysta")
-    # else:
-    #     print("Liczba jest nieparzysta")
-
-    # 6. Operacje arytmetyczne
-    # a = 4
-    # b = 2
-    # c = a + b
-    # print("c = ", c)
-    #
-    # c = a - b
-    # print("c = ", c)
-    #
-    # c = a * b
-    # print("c = ", c)
-    #
-    # c = a / b
-    # print("c = ", c)
-    #
-    # c = a**2
-    # print("c = ", c)
-
-
-    # "Nigdy mi się nie przyda rozwiązywanie równań kwadratowych!"
-    # 7. Napisz program rozwiązujący równanie kwadratowe (o ile istnieje)
-
-    # https://www.matemaks.pl/rownania-kwadratowe-w-postaci-ogolnej.html
-
-    # a = 1
-    # b = 0
-    # c = 0
-
-    # delta = b^2 - 4ac
-    # delta = b**2 - 4 * a * c
-    # print(delta)
-
-    # if (delta < 0):
-    #     print("Rownanie kwadratowe nie ma rozwiazania")
-    # elif(delta > 0):
-    #     math.sqrt(delta)
-    #     print("To macie napisać")
-    # else:
-    #     x = -b / 2*a
-    #     print(x)
-
 
 
     # 8. Wczytywanie danych z klawiatury
