64ca35cbba7382b8c255f12441a8841b6c067998
Przemysław <przemyslawjakubowski@gmail.com>
2022-04-28 14:50:48 +0200

    Lekcja 3

--- lekcja3/main.py
+++ lekcja3/main.py
@@ -17,62 +17,61 @@ def drukujPole(a):
     print(a**2)
 
 
-if __name__ == '__main__':
-    # Przerabiamy do Pythona
-    # 1. https://scratch.mit.edu/projects/681784124/editor/
-    # 2. https://scratch.mit.edu/projects/681784964/editor/
-    # 3. https://scratch.mit.edu/projects/681785553/editor/
-    # 4. https://scratch.mit.edu/projects/681804535/editor/
-    # 5. https://scratch.mit.edu/projects/681806599/editor/
-    # 6. https://scratch.mit.edu/projects/681786757/editor/
-    # 7. https://scratch.mit.edu/projects/681792391/editor/
-    # 8. https://scratch.mit.edu/projects/681797853/editor/
-    # 9. https://scratch.mit.edu/projects/681811993/editor/
+# Przerabiamy do Pythona
+# 1. https://scratch.mit.edu/projects/681784124/editor/
+# 2. https://scratch.mit.edu/projects/681784964/editor/
+# 3. https://scratch.mit.edu/projects/681785553/editor/
+# 4. https://scratch.mit.edu/projects/681804535/editor/
+# 5. https://scratch.mit.edu/projects/681806599/editor/
+# 6. https://scratch.mit.edu/projects/681786757/editor/
+# 7. https://scratch.mit.edu/projects/681792391/editor/
+# 8. https://scratch.mit.edu/projects/681797853/editor/
+# 9. https://scratch.mit.edu/projects/681811993/editor/
 
-    # 2. W Scratchu można dodawać tylko dane wejściowe. Nas może interesować też żeby te metody coś
-    #   zwracały.
-    # policzone = policzSume(8, 5)
-    # print(policzone)
+# 2. W Scratchu można dodawać tylko dane wejściowe. Nas może interesować też żeby te metody coś
+#   zwracały.
+# policzone = policzSume(8, 5)
+# print(policzone)
 
-    # Napisz metodę, która policzy pole kwadratu i zwróci tą wartość. Pole kwadratu a**2
-    # Wypisz wartość
+# Napisz metodę, która policzy pole kwadratu i zwróci tą wartość. Pole kwadratu a**2
+# Wypisz wartość
 
-    # Nie musimy tworzyć zmiennej
-    print(policzSume(10, 20))
+# Nie musimy tworzyć zmiennej
+print(policzSume(10, 20))
 
-    # Wywołaj swoją metodę do liczenia pola kwadratu i wypisz jej wynik bez tworzenia zmiennej na wynik
+# Wywołaj swoją metodę do liczenia pola kwadratu i wypisz jej wynik bez tworzenia zmiennej na wynik
 
-    # 3. Za pomocą metody poleKwadratu() oblicz sumę pól kwadratów o bokach długości 5 i 10
-    # sumaPol = poleKwadratu(5) + poleKwadratu(10)
-    # print("sumaPol", sumaPol)
+# 3. Za pomocą metody poleKwadratu() oblicz sumę pól kwadratów o bokach długości 5 i 10
+# sumaPol = poleKwadratu(5) + poleKwadratu(10)
+# print("sumaPol", sumaPol)
 
-    # Przerób swoją metodę do liczenia pola trójkąta tak, aby zwracała tą wartość. Policz i wypisz sumę pól trójkątów:
-    # a = 1 h = 2, a = 2 h = 4, a = 5 b = 2
+# Przerób swoją metodę do liczenia pola trójkąta tak, aby zwracała tą wartość. Policz i wypisz sumę pól trójkątów:
+# a = 1 h = 2, a = 2 h = 4, a = 5 b = 2
 
-    ##########
-    # Zadanie
-    # 1. Napisz metodę, która podzieli przez siebie dwie liczby i zwróci wynik
-    # 2. Napisz metodę, która obliczy pole koła. Poszukaj jak zdobyć wartość PI - biblioteka math
-    # 3. Napisz metodę, która sprawdzi czy dana liczba jest parzysta czy nie parzysta
-    #     i zwróci tą informację
-    # 4. Za pomocą pierwszej metody przeprowadź działanie :
-    # (14 / 7) + (6 / 3)
-    # i wypisz jego wynik
-    #
-    # 5. Należy przerobić ten program Scratchowy do Pythona:
-    # https://scratch.mit.edu/projects/678920938/editor/
-
+##########
+# Zadanie
+# 1. Napisz metodę, która podzieli przez siebie dwie liczby i zwróci wynik
+# 2. Napisz metodę, która obliczy pole koła. Poszukaj jak zdobyć wartość PI - biblioteka math
+# 3. Napisz metodę, która sprawdzi czy dana liczba jest parzysta czy nie parzysta
+#     i zwróci tą informację
+# 4. Za pomocą pierwszej metody przeprowadź działanie :
+# (14 / 7) + (6 / 3)
+# i wypisz jego wynik
+#
+# 5. Należy przerobić ten program Scratchowy do Pythona:
+# https://scratch.mit.edu/projects/678920938/editor/
 
 
-    # Czytanie z klawiatury - łącznie z przerabianiem na int(). Zadanie napisać guessTheNumber(liczba)
-    # Liczba jest losowa od 0 do 100 random.randint()
-    # Różne metody wbudowane - random.randint()
-    # https://scratch.mit.edu/projects/678924474/editor/
-    # Kolekcje
-    # List, Set, Dictionary
-    # Policz sumę liczb w liście
-    # Obsługa wyjątków
-    # drukujPole(5)
-    #
-    # bok = 10
-    # drukujPole(bok)
+
+# Czytanie z klawiatury - łącznie z przerabianiem na int(). Zadanie napisać guessTheNumber(liczba)
+# Liczba jest losowa od 0 do 100 random.randint()
+# Różne metody wbudowane - random.randint()
+# https://scratch.mit.edu/projects/678924474/editor/
+# Kolekcje
+# List, Set, Dictionary
+# Policz sumę liczb w liście
+# Obsługa wyjątków
+# drukujPole(5)
+#
+# bok = 10
+# drukujPole(bok)
