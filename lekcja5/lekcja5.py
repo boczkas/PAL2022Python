29645fba1d3b03539c0e7cafa4ac381db725d41a
Przemysław <przemyslawjakubowski@gmail.com>
2022-05-16 14:15:24 +0200

    lekcja 6

--- lekcja5/lekcja5.py
+++ lekcja5/lekcja5.py
@@ -1,6 +1,20 @@
 # Mam mieć kalkulator w osobnym pliku
+
+
+# 1. Chce przerobic na liczba, a wisuje szyszka
+# tekst = input("Jaka liczba?")
+# liczba = int(tekst)
+# print(liczba)
 # Obsługa wyjątków
 
+# 2. Wyjatek
+
+tekst = input("Jaka liczba?")
+try:
+    liczba = int(tekst)
+    print(liczba)
+except:
+    print("To nie liczba")
 # Liczba jest losowa od 0 do 100 random.randint()
 # Różne metody wbudowane - random.randint()
 # https://scratch.mit.edu/projects/678924474/editor/
---
+++ lekcja6/main.py
@@ -0,0 +1,22 @@
+def dlugie(a,b,c,d,e,f):
+    print(a,b,c,d,e,f,)
+
+
+def przyjmujeTablice(tablica):
+    for element in tablica:
+        print(element)
+
+
+# 1. Napisz metodę, która przyjmuje 6 argumentów i wypisuej je
+# dlugie("q", "w", "e", "r", "t", "y")
+
+# 2. To musi być leiej ogarnięte, ano jest - mamy Array
+elo = ["a", "s", "d", "f", "g", "h", "i", "j", "kaczka"]
+
+for i in elo:
+    print(i)
+
+# przyjmujeTablice(elo)
+
+# Napisz metodę, która przyjmuję talicę z liczbami od 0 do 10 i zwraca ich sumę.
+# Wypisz wartość zwracaną przez tą metodę
