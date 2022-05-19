7981a5bf4d2fe95f4f1ca272acbff53bcdd2f066
Przemysław <przemyslawjakubowski@gmail.com>
2022-05-19 13:30:02 +0200

    lekcja 6

--- lekcja5/lekcja5.py
+++ lekcja5/lekcja5.py
@@ -1,6 +1,3 @@
-# Mam mieć kalkulator w osobnym pliku
-
-
 # 1. Chce przerobic na liczba, a wisuje szyszka
 # tekst = input("Jaka liczba?")
 # liczba = int(tekst)
@@ -9,16 +6,37 @@
 
 # 2. Wyjatek
 
-tekst = input("Jaka liczba?")
-try:
-    liczba = int(tekst)
-    print(liczba)
-except:
-    print("To nie liczba")
+# tekst = input("Jaka liczba?")
+# try:
+#     liczba = int(tekst)
+#     print(liczba)
+# except:
+#     print("To nie liczba")
+
+# 3. Napisz program, który będzie wczytywał liczbę i ją wypisywał.
+#   Jeżeli użytkownik poda niewłaściwą wartość program go o tym poinformuje i ponownie
+#   poprosi o wpisanie liczby.
+#   Program ma działać w nieskończoność.
+#   Jeżeli użytkownik wpisze "q" program ma się zakończyć.
+
+# while True:
+#     tekst = input("Jaka liczba?")
+#     if tekst == "q":
+#         print("Koncze!")
+#         break
+#     try:
+#         liczba = int(tekst)
+#         print("Liczba to:", liczba)
+#     except:
+#         print(tekst, " - to nie liczba")
+
+# 4. Przerób swój kalkulator tak, aby obsługiwał błędnie wpisane liczby czy działanie
+#   Jeśli nie masz kalkulatora to rób kalkulator :)
+
+# 5. GuessTheNumber
 # Liczba jest losowa od 0 do 100 random.randint()
 # Różne metody wbudowane - random.randint()
 # https://scratch.mit.edu/projects/678924474/editor/
 # Kolekcje
 # List, Set, Dictionary
 # Policz sumę liczb w liście
-# Obsługa wyjątków
--- lekcja6/main.py
+++ lekcja6/main.py
@@ -11,12 +11,28 @@ def przyjmujeTablice(tablica):
 # dlugie("q", "w", "e", "r", "t", "y")
 
 # 2. To musi być leiej ogarnięte, ano jest - mamy Array
-elo = ["a", "s", "d", "f", "g", "h", "i", "j", "kaczka"]
-
-for i in elo:
-    print(i)
+# elo = ["a", "s", "d", "f", "g", "h", "i", "j", "kaczka"]
+#
+# for i in elo:
+#     print(i)
 
 # przyjmujeTablice(elo)
 
-# Napisz metodę, która przyjmuję talicę z liczbami od 0 do 10 i zwraca ich sumę.
-# Wypisz wartość zwracaną przez tą metodę
+# 3. Pokazówka
+tablicaCebul = ["zwykla", "bardzoRosnie", "zwykla", "troszkeRosnie", "rosnie"]
+
+for cebula in tablicaCebul:
+    print(cebula)
+
+
+# 3. Wiemy po co to teraz się z nimi pobawmy
+liczby = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+suma = 0
+for liczba in liczby:
+    print(liczba)
+    suma += liczba
+print("suma =", suma)
+
+# 4. Policz ile jest piątek w tablicy
+tablica = [0, 1, 2, 5, 3, 6, 5, 5, 5]
